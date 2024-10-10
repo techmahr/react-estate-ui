21448ba46bd415919a245f2cf90c7dba7bbe8f27
Mahroof K <mahroofk@Mahroofs-MacBook-Pro.local>
2024-10-10 20:56:23 +0530

    navbar changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Navbar from "./components/navbar/Navbar"
+
 import './index.css';
 import './layout.css';
 
 function App() {
   return (
     <div className='layout'>
-      Hello
+      <Navbar/>
     </div>
   )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./navbar.css";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav>
+      <div className="left">
+        <a className="logo" href="/">
+          <img src="/logo.png" alt="" />
+          <span>MahrEstate</span>
+        </a>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Agents</a>
+      </div>
+      <div className="right">
+        <a href="#">Sign in</a>
+        <a href="#" className="register">
+          Sign up
+        </a>
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Agents</a>
+          <a href="#">Sign in</a>
+          <a href="#">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
